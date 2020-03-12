c99bfb4c74021f5ee0fa22586ae43daa0c2a50f2
ZackFra <zack11frazier22@gmail.com>
2020-03-12 02:57:13 +0000

    update

--- devsetup.sh
+++ devsetup.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
 
-# install rmate
+# install rmate ref: https://github.com/randy3k/RemoteSubl
 curl -o /usr/local/bin/rmate https://raw.githubusercontent.com/aurora/rmate/master/rmate
 sudo chmod +x /usr/local/bin/rmate
 mv /usr/local/bin/rmate /usr/local/bin/rsubl
 
 
-# install nvm
+# install nvm ref: https://github.com/nvm-sh/nvm
 curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.35.3/install.sh | bash
 export NVM_DIR="$([ -z "${XDG_CONFIG_HOME-}" ] && printf %s "${HOME}/.nvm" || printf %s "${XDG_CONFIG_HOME}/nvm")"
 [ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh" # This loads nvm
