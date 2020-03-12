a0dce0aee0661c9cb7748ffa018df7906d3d7b36
ZackFra <zack11frazier22@gmail.com>
2020-03-12 03:33:37 +0000

    installs mongo shell

--- devsetup.sh
+++ devsetup.sh
@@ -11,6 +11,13 @@ curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.35.3/install.sh | bash
 export NVM_DIR="$([ -z "${XDG_CONFIG_HOME-}" ] && printf %s "${HOME}/.nvm" || printf %s "${XDG_CONFIG_HOME}/nvm")"
 [ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh" # This loads nvm
 
+# install mongo shell ref: https://docs.mongodb.com/manual/tutorial/install-mongodb-on-ubuntu/
+wget -qO - https://www.mongodb.org/static/pgp/server-4.2.asc | sudo apt-key add -
+echo "deb [ arch=amd64,arm64 ] https://repo.mongodb.org/apt/ubuntu bionic/mongodb-org/4.2 multiverse" | sudo tee /etc/apt/sources.list.d/mongodb-org-4.2.list
+sudo apt-get install -y mongodb-org
+
+# update + cleanup
 sudo apt update
 sudo apt upgrade -y
 sudo apt autoremove
+
