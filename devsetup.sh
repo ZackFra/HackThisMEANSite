a85131593f8995f998887d3da24d3c2b7a08956c
= <zack11frazier22@gmail.com>
2020-03-12 01:23:07 +0000

    added setup script

---
+++ devsetup.sh
@@ -0,0 +1,15 @@
+#!/usr/bin/env bash!
+
+# install sublime
+wget -qO - https://download.sublimetext.com/sublimehq-pub.gpg | sudo apt-key add -
+sudo apt-get install apt-transport-https
+echo "deb https://download.sublimetext.com/ apt/stable/" | sudo tee /etc/apt/sources.list.d/sublime-text.list
+
+# install nvm
+curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.35.3/install.sh | bash
+export NVM_DIR="$([ -z "${XDG_CONFIG_HOME-}" ] && printf %s "${HOME}/.nvm" || printf %s "${XDG_CONFIG_HOME}/nvm")"
+[ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh" # This loads nvm
+
+sudo apt update
+sudo apt upgrade -y
+sudo apt-get install sublime-text -y
