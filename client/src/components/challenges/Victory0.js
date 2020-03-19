f0ba02a0cbd0aa2dba91567abb8be5649ef01a3e
ZackFra <zack11frazier22@gmail.com>
2020-03-19 02:17:32 +0000

    removed devsetup

--- client/src/components/challenges/Victory0.js
+++ client/src/components/challenges/Victory0.js
@@ -5,7 +5,7 @@ function Victory0() {
 	return (
 		<Container>
 			<h1 className="pb-2 mt-4 border-bottom text-success" style={{padding: '1rem 0'}}>CONGRATULATIONS! :)</h1>
-			<p className="text-success">You passed level 0!</p>
+			<p className="text-success">You hacked challenge 0!</p>
 		</Container>
 	);
 }
