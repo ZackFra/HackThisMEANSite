ffa89b991a4abda612abd3419668f525cbdb854d
ZackFra <zack11frazier22@gmail.com>
2020-04-14 11:47:32 +0000

    added challenge3 stuff

--- client/src/components/challenges/Challenge3.js
+++ client/src/components/challenges/Challenge3.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Container, Button } from 'reactstrap';
+import { Container, Button, Label } from 'reactstrap';
 import { useSelector, useDispatch } from 'react-redux';
 
 function Challenge3(props) {
@@ -11,62 +11,261 @@ function Challenge3(props) {
 		e.preventDefault();
 		let re = /\s/;
 		if(password === '') {
-			document.getElementById('invalReg').innerText='Passwords cannot be blank.';
+			document.getElementById('inval').innerText='Password cannot be blank.\n\n';
+		} else if(username === '') {
+			document.getElementById('inval').innerText='User name cannot be blank.\n\n';
 		} else if(password !== confirm) {
-			document.getElementById('invalReg').innerText='Passwords must match.';
+			document.getElementById('inval').innerText='Passwords must match.\n\n';
 		} else if(password.length < 8) {
-			document.getElementById('invalReg').innerText='Passwords must be at least 8 characters long.';
+			document.getElementById('inval').innerText='Password must be at least 8 characters long.\n\n';
 		} else if(re.test(password)) {
-			document.getElementById('invalReg').innerText='Passwords may not contain white spaces.';
-		} else {
-			// @todo add actual call to back-end
-			localStorage.setItem('username', {username, password});
+			document.getElementById('inval').innerText='Password may not contain white spaces.\n\n';
+		} else if(re.test(username)) {
+			document.getElementById('inval').innerText='User name may not contain white spaces.\n\n';
+		}
+		else {
+			// @todo add actually call to back-end
+			localStorage.setItem('account', JSON.stringify({username, password}));
 			window.location.reload();
 		}
 	}
 
+	function login(e) {
+		e.preventDefault();
+		let acc = JSON.parse(localStorage.getItem('account'));
+		if(username === acc.username && password === acc.password) {
+			document.cookie += `username=${username}`;
+			window.location.reload();
+		} else {
+			document.getElementById('inval').innerText = 'Incorrect password.\n\n';
+		}
+		// @todo add login functionality
+	}
+
+	// clear any errors
+	function clear() {
+		document.getElementById('inval').innerText = '';
+	}
+
+	function changePass(e) {
+		let re = /\s/;
+		if(password === '') {
+			document.getElementById('inval').innerText='Password cannot be blank.\n\n';
+		} else if(password !== confirm) {
+			document.getElementById('inval').innerText='Passwords must match.\n\n';
+		} else if(password.length < 8) {
+			document.getElementById('inval').innerText='Password must be at least 8 characters long.\n\n';
+		} else if(re.test(password)) {
+			document.getElementById('inval').innerText='Password may not contain white spaces.\n\n';
+		} else if(username === 'admin') {
+			window.location = '/Victory3';
+		} else {
+			// do change password stuff
+		}
+	}
+
 	switch(tab) {
+		case 'CHANGE_PASS':
+			return (
+				<Container>
+					<h1 className="pb-2 mt-4 border-bottom" style={{padding: '1rem 0'}}>Welcome to Challenge 3</h1>
+						<br />
+						<Container>
+							<div className="card" style={{width: '50%', margin: 'auto'}}>
+								<div className="card-body bg-dark text-light">
+									<div className="card-title">
+										Change Password
+									</div>
+									<hr color="lightgray"/>
+									<div className="card-text">
+										<div className="text-warning" id="inval" />
+										<form className="form-group" onSubmit={register}>								
+											<Label for='passReg' style={{'margin':'0','float': 'left', 'width': '30%', 'marginRight': '0'}}>Password: </Label>
+											<input 
+												value={password}
+												onChange={e => dispatch({type: 'UPDATE_PASS', payload: e.target.value})}
+												className="form-control" 
+												type="password" 
+												placeholder="password" 
+												style={{'margin': '0', 'width': '70%'}} />
+
+											<Label for='confirm' style={{'margin': '0.4rem 0 0 0','float': 'left', 'width': '30%'}}>Password (confirm): </Label>
+											<input 
+												id='confirm'
+												value={confirm}
+												onChange={e => dispatch({type: 'UPDATE_CONFIRM', payload: e.target.value})}
+												className="form-control" 
+												type="password" 
+												placeholder="confirm password" 
+												style={{'marginTop': '0.4rem', 'width': '70%'}} />
+											<Button color="primary" type="submit" style={{'marginLeft': '86%', 'marginTop': '0.4rem'}}>Submit</Button>
+										</form>
+										<div style={{'marginLeft': '5%'}}>
+											<Button 
+												color="link" 
+												onClick={() => {
+													clear();
+													dispatch({type: 'CLEAR_ALL'}); 
+													dispatch({type: 'SET_TAB', payload: 'STANDARD'})
+											}}>home</Button>
+											<Button 
+												color="link" 
+												onClick={() => {
+													clear();
+													dispatch({type: 'CLEAR_ALL'}); 
+													dispatch({type: 'SET_TAB', payload: 'REGISTER'})
+											}}>register</Button>
+											<Button
+												color="link"
+												onClick={() => {
+													clear();
+													dispatch({type: 'CLEAR_ALL'}); 
+													dispatch({type: 'SET_TAB', payload: 'LOGIN'});
+											}}>login</Button>
+											<Button 
+												color="link" 
+												onClick={() => {
+													clear();
+													dispatch({type: 'CLEAR_ALL'}); 
+													dispatch({type: 'SET_TAB', payload: 'ACCOUNT'})
+											}}>account</Button>
+										</div>
+									</div>
+									<div className='text-success' id="success" />
+									<br />
+								</div>
+							</div>
+						</Container>
+					</Container>
+				);	
+		case 'ACCOUNT':
+			return (
+				<Container>
+					<h1 className="pb-2 mt-4 border-bottom" style={{padding: '1rem 0'}}>Welcome to Challenge 3</h1>
+						<br />
+						<Container>
+							<div className="card" style={{width: '50%', margin: 'auto'}}>
+								<div className="card-body bg-dark text-light">
+									<div className="card-title">
+										Account
+									</div>
+									<hr color="lightgray"/>
+									<div className="card-text">
+										<Label for='userAcc' style={{'float':'left', 'width': '60%'}}>Username: </Label>
+										<div id='userAcc'>Placeholder</div>
+										<br />
+										<Label for='since' style={{'float':'left', 'width': '60%'}}>User since: </Label>
+										<div id='since'>Placeholder</div>
+										<br />
+										<Label for='reset' style={{'float': 'left', 'width': '60%'}}>Reset Password: </Label>
+										<Button 
+											id='reset' 
+											onClick={ () => 
+												dispatch({type: 'SET_TAB', payload: 'CHANGE_PASS'})
+										}>Reset</Button>
+										<br />
+										<br />
+										<div style={{'marginLeft': '5%'}}>
+											<Button 
+												color="link" 
+												onClick={() => {
+													dispatch({type: 'CLEAR_ALL'}); 
+													dispatch({type: 'SET_TAB', payload: 'STANDARD'})
+												}}>home</Button>
+											<Button 
+												color="link" 
+												onClick={() => {
+													dispatch({type: 'CLEAR_ALL'}); 
+													dispatch({type: 'SET_TAB', payload: 'REGISTER'})
+											}}>register</Button>
+											<Button
+												color="link"
+												onClick={() => {
+													dispatch({type: 'CLEAR_ALL'}); 
+													dispatch({type: 'SET_TAB', payload: 'LOGIN'});
+											}}>login</Button>
+											<Button 
+												color="link" 
+												onClick={() => {
+													dispatch({type: 'CLEAR_ALL'}); 
+													dispatch({type: 'SET_TAB', payload: 'ACCOUNT'})
+											}}>account</Button>
+										</div>
+									</div>
+									<div className='text-success' id="success"></div>
+									<br />
+								</div>
+							</div>
+						</Container>
+					</Container>
+
+			);
 		case 'LOGIN':
 			return (
 				<Container>
-					<h1 className="pb-2 mt-4 border-bottom" style={{padding: '1rem 0'}}>Welcome to Challenge 4</h1>
+					<h1 className="pb-2 mt-4 border-bottom" style={{padding: '1rem 0'}}>Welcome to Challenge 3</h1>
 						<br />
 						<Container>
-							<div className="card" style={{width: '18rem', margin: 'auto'}}>
+							<div className="card" style={{width: '50%', margin: 'auto'}}>
 								<div className="card-body bg-dark text-light">
 									<div className="card-title">
 										Log in
 									</div>
 									<hr color="lightgray"/>
 									<div className="card-text">
-										<div id="invalLog" />
-										<form className="form-group">
+										<div className='text-warning' id="inval" />
+										<form className="form-group" onSubmit={login}>
+											<Label for='userLog' style={{'float': 'left', 'width': '30%'}}>Username: </Label>
 											<input 
+												id='userLog'
 												value={username} 
 												className="form-control" 
 												placeholder="username" 
-												style={{'marginTop': '0.2rem'}} 
+												style={{'width': '70%'}} 
 												onChange= { e => dispatch({type: 'UPDATE_USER', payload: e.target.value})} 
 											/>
+											<Label for='passLog' style={{'marginTop': '0.4rem', 'float': 'left', 'width': '30%'}}>Password: </Label>
 											<input 
+												id='passLog'
 												value={password} 
 												className="form-control" 
 												type="password" 
 												placeholder="password" 
-												style={{'marginTop': '0.4rem'}} 
+												style={{'marginTop': '0.4rem', 'width': '70%'}} 
 												onChange={e => dispatch({type: 'UPDATE_PASS', payload: e.target.value})}
 											/>
-											<Button color="primary" type="submit" style={{'marginLeft': '69%', 'marginTop': '0.4rem', 'display': 'inline-block'}}>Submit</Button>
+											<br />
+											<Button color="primary" type="submit" style={{'marginLeft': '86%', 'marginTop': '0.4rem', 'display': 'inline-block'}}>Submit</Button>
 										</form>
-										<div style={{marginLeft: '10%'}}>
+										<div style={{marginLeft: '5%'}}>
 											<Button 
 												color="link" 
 												onClick={() => {
+													clear();
 													dispatch({type: 'CLEAR_ALL'}); 
 													dispatch({type: 'SET_TAB', payload: 'STANDARD'})
-												}}>return
-											</Button>
-											<Button color="link" onClick={() => {dispatch({type: 'CLEAR_ALL'}); dispatch({type: 'SET_TAB', payload: 'REGISTER'})}}>register</Button>
+												}}>home</Button>
+											<Button 
+												color="link" 
+												onClick={() => {
+													clear();
+													dispatch({type: 'CLEAR_ALL'}); 
+													dispatch({type: 'SET_TAB', payload: 'REGISTER'})
+											}}>register</Button>
+											<Button
+												color="link"
+												onClick={() => {
+													clear();
+													dispatch({type: 'CLEAR_ALL'}); 
+													dispatch({type: 'SET_TAB', payload: 'LOGIN'});
+											}}>login</Button>
+											<Button 
+												color="link" 
+												onClick={() => {
+													clear();
+													dispatch({type: 'CLEAR_ALL'}); 
+													dispatch({type: 'SET_TAB', payload: 'ACCOUNT'})
+											}}>account</Button>
 										</div>
 									</div>
 									<div className='text-success' id="success"></div>
@@ -80,56 +279,79 @@ function Challenge3(props) {
 		case 'REGISTER':
 			return (
 				<Container>
-					<h1 className="pb-2 mt-4 border-bottom" style={{padding: '1rem 0'}}>Welcome to Challenge 4</h1>
+					<h1 className="pb-2 mt-4 border-bottom" style={{padding: '1rem 0'}}>Welcome to Challenge 3</h1>
 						<br />
 						<Container>
-							<div className="card" style={{width: '18rem', margin: 'auto'}}>
+							<div className="card" style={{width: '50%', margin: 'auto'}}>
 								<div className="card-body bg-dark text-light">
 									<div className="card-title">
 										Register
 									</div>
 									<hr color="lightgray"/>
 									<div className="card-text">
-										<div className="text-warning" id="invalReg" />
+										<div className="text-warning" id="inval" />
 										<form className="form-group" onSubmit={register}>
+											<Label for='userReg' style={{'float': 'left', 'width': '35%'}}>Username: </Label>
 											<input 
+												id='userReg'
 												value={username}
 												onChange={e => dispatch({type: 'UPDATE_USER', payload: e.target.value})}
 												className="form-control" 
 												placeholder="username" 
-												style={{'marginTop': '0.2rem'}} />
+												style={{'marginTop': '0.2rem', 'width': '65%'}} />
+																					
+											<Label for='passReg' style={{'marginTop':'0.5rem','float': 'left', 'width': '35%'}}>Password: </Label>
 											<input 
 												value={password}
 												onChange={e => dispatch({type: 'UPDATE_PASS', payload: e.target.value})}
 												className="form-control" 
 												type="password" 
 												placeholder="password" 
-												style={{'marginTop': '0.4rem'}} />
+												style={{'marginTop': '0.4rem', 'width': '65%'}} />
+
+											<Label for='confirm' style={{'marginTop': '0.5rem','float': 'left', 'width': '35%'}}>Password (confirm): </Label>
 											<input 
+												id='confirm'
 												value={confirm}
 												onChange={e => dispatch({type: 'UPDATE_CONFIRM', payload: e.target.value})}
 												className="form-control" 
 												type="password" 
 												placeholder="confirm password" 
-												style={{'marginTop': '0.4rem'}} />
-											<Button color="primary" type="submit" style={{'marginLeft': '69%', 'marginTop': '0.4rem'}}>Submit</Button>
+												style={{'marginTop': '0.4rem', 'width': '65%'}} />
+											<Button color="primary" type="submit" style={{'marginLeft': '86%', 'marginTop': '0.4rem'}}>Submit</Button>
 										</form>
-										<div style={{marginLeft: '10%'}}>
-											<Button 
-												color="link" 
-												onClick={ () => {
-													dispatch({type: 'SET_TAB', payload: 'STANDARD'});
-													dispatch({type: 'CLEAR_ALL'});
-											}}>return</Button>
+										<div style={{'marginLeft': '5%'}}>
 											<Button 
 												color="link" 
 												onClick={() => {
+													clear();
+													dispatch({type: 'CLEAR_ALL'}); 
+													dispatch({type: 'SET_TAB', payload: 'STANDARD'})
+											}}>home</Button>
+											<Button 
+												color="link" 
+												onClick={() => {
+													clear();
+													dispatch({type: 'CLEAR_ALL'}); 
+													dispatch({type: 'SET_TAB', payload: 'REGISTER'})
+											}}>register</Button>
+											<Button
+												color="link"
+												onClick={() => {
+													clear();
+													dispatch({type: 'CLEAR_ALL'}); 
 													dispatch({type: 'SET_TAB', payload: 'LOGIN'});
-													dispatch({type: 'CLEAR_ALL'});
 											}}>login</Button>
+											<Button 
+												color="link" 
+												onClick={() => {
+													clear();
+													dispatch({type: 'CLEAR_ALL'}); 
+													dispatch({type: 'SET_TAB', payload: 'ACCOUNT'})
+											}}>account</Button>
 										</div>
 									</div>
-									<div className='text-success' id="success"></div>
+									<div className='text-success' id="success" />
 									<br />
 								</div>
 							</div>
@@ -142,28 +364,43 @@ function Challenge3(props) {
 					<h1 className="pb-2 mt-4 border-bottom" style={{padding: '1rem 0'}}>Welcome to Challenge 3</h1>
 						<br />
 						<Container>
-							<div className="card" style={{width: '18rem', margin: 'auto'}}>
+							<div className="card" style={{width: '40%', margin: 'auto'}}>
 								<div className="card-body bg-dark text-light">
 									<div className="card-title">
-										Cookie Stealing
+										Session Hi-Jacking
 									</div>
 									<hr color="lightgray"/>
 									<div className="card-text">
 										This site has been hi-jacked by some script kiddies. 
-										You're job is to jack it back. See if you can't find a
+										Your job is to jack it back. See if you can't find a
 										way to reclaim the admin account.
+										<br />
 										<br />
 										<div style={{marginLeft: '10%'}}>
 											<Button 
 												color="link" 
-												onClick={() => dispatch({type: 'SET_TAB', payload: 'REGISTER'})}
-												>register
-											</Button>
+												onClick={() => {
+													dispatch({type: 'CLEAR_ALL'}); 
+													dispatch({type: 'SET_TAB', payload: 'STANDARD'})
+											}}>home</Button>
 											<Button 
 												color="link" 
-												onClick={() => dispatch({type: 'SET_TAB', payload: 'LOGIN'})}
-												>login
-											</Button>
+												onClick={() => {
+													dispatch({type: 'CLEAR_ALL'}); 
+													dispatch({type: 'SET_TAB', payload: 'REGISTER'})
+											}}>register</Button>
+											<Button
+												color="link"
+												onClick={() => {
+													dispatch({type: 'CLEAR_ALL'}); 
+													dispatch({type: 'SET_TAB', payload: 'LOGIN'});
+											}}>login</Button>
+											<Button 
+												color="link" 
+												onClick={() => {
+													dispatch({type: 'CLEAR_ALL'}); 
+													dispatch({type: 'SET_TAB', payload: 'ACCOUNT'})
+											}}>account</Button>
 										</div>
 									</div>
 									<div className='text-success' id="success"></div>
