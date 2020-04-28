4c62bacf58c3341039584235e008f62e87a63ff8
ZackFra <zack11frazier22@gmail.com>
2020-04-28 18:56:34 +0000

    added title font

--- client/src/StyleSheet.js
+++ client/src/StyleSheet.js
@@ -1,5 +1,6 @@
 import React from 'react';
 
+
 export function Title(props) {
-	return <h1 className="pb-2 mt-4 border-bottom title-color" style={{padding: '1rem 0'}}>{props.title}</h1>
+	return <h1 className="pb-2 mt-4 border-bottom title-color" style={{padding: '1rem 0', fontFamily: 'Permanent Marker', fontStyle: 'italic'}}>{props.title}</h1>
 }
